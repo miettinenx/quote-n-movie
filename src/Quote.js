88e090d685085372c970103666b6e54d973e088f
miettinenx <miettinenmm@elisanet.fi>
2017-11-19 19:10:05 +0200

    fetching data corrections

--- src/Quote.js
+++ src/Quote.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Movie from './Movie.js';
-import QuotesJSON from './json/quotes.json';
+//import QuotesJSON from './json/quotes.json';
 import { Table } from 'react-bootstrap'
 
 class Quote extends Component {
@@ -9,6 +9,7 @@ class Quote extends Component {
         this.state = {
           quotes: []
         };
+
     }
     /**
      * Sorts quoteJSON to descending order
@@ -16,13 +17,47 @@ class Quote extends Component {
      * @param {Number} b
      */
     sortQuotes() {
-        QuotesJSON.sort(function(a, b){
+        var sortQuotesArr = this.state.quotes;
+        console.log(sortQuotesArr);
+        sortQuotesArr.sort(function(a, b){
             return b.score - a.score;
+        });
+    //    console.log(QuotesJSON);
+        this.setState({
+            quotes:sortQuotesArr
         });
     }
     componentDidMount() {
-         this.sortQuotes();
-         this.setState({quotes: QuotesJSON});
+        this.getQuoteData();
+        ////////////////
+         //this.setState({quotes: QuotesJSON});
+    }
+
+
+    getQuoteData(){
+        var request = 'https://mediasignal-quotes.herokuapp.com/quotes';
+        ////////////////
+        var proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+        var that = this;
+        fetch(proxyUrl + request)
+        .then(
+        function(response) {
+            if (response.status !== 200) {
+                console.log('Looks like there was a problem. Status Code: ' +
+                response.status);
+                return;
+            }
+            // Examine the text in the response
+            response.json().then(function(data) {
+                //console.log('daa', data);
+                that.setState({quotes: data});
+                that.sortQuotes();
+                //console.log('daa', that.state.quotes);
+            });
+        }).catch(function(err) {
+            console.log('Fetch Error :-S', err);
+        });
+
     }
     render() {
         let quotes = this.state.quotes;
